4f7e7b46f54183450599659a5346da61aa9c3f66
Minecraft0088 <wiktorasonovic@outlook.com>
2024-09-13 03:36:20 +0400

    LOGGER #5155

---
+++ Logger/Logger/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+	internal class Program
+	{
+		
+		static void Main(string[] args)
+		{
+
+			logger logger = new logger("log.txt");
+			
+
+			string Keypress()
+			{
+				if (Console.ReadLine() == "a") 
+				{
+					logger.Log("Pressed a");
+					
+				}else
+				{
+					logger.Log("Not pressed a");
+					
+				}
+				return "";
+			}
+
+			Keypress();
+
+			try
+			{
+				logger.Log("Application started");
+				
+				int a = 10;
+				int result = a / 0;
+				// Simulating an error
+				//throw new InvalidOperationException("Something went wrong!");
+			}
+			catch (Exception ex)
+			{
+				logger.LogError(ex);
+				Console.WriteLine("YOU HAVE ERROR " + ex.ToString());
+			}
+			finally
+			{
+				logger.Log("Application ended");
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Logger/Logger/logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+public class logger
+{
+	private readonly string logFilePath;
+
+	public logger(string filePath)
+	{
+		logFilePath = filePath;
+	}
+
+	public void Log(string message)
+	{
+		string logEntry = $"{DateTime.Now}: {message}";
+		File.AppendAllText(logFilePath, logEntry + Environment.NewLine);
+		Console.WriteLine(logEntry); // Optionally, log to console too
+	}
+
+	public void LogError(Exception ex)
+	{
+		string errorLog = $"{DateTime.Now}: ERROR - {ex.Message}{Environment.NewLine}{ex.StackTrace}";
+		File.AppendAllText(logFilePath, errorLog + Environment.NewLine);
+		Console.WriteLine(errorLog); // Optionally, log to console too
+	}
+}
